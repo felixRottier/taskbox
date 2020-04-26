a984fb35c17ae073923ad8f9153595a31329f77e
Félix <felix.rottier@gmail.com>
2020-04-26 21:14:55 +0200

    taskbox UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux';
+import store from './lib/redux';
 
+import InboxScreen from './components/InboxScreen';
+
+import './index.css';
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
