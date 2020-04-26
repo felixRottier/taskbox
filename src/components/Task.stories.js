f2dfcaf06cd303ef5155b1db92b009e30de9bcea
Félix <felix.rottier@gmail.com>
2020-04-26 22:05:22 +0200

    taskbox UI

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import Task from './Task';
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
 
 export default {
   component: Task,
   title: 'Task',
+  decorators: [withKnobs],
   excludeStories: /.*Data$/,
 };
 
@@ -22,7 +26,7 @@ export const actionsData = {
 };
 
 export const Default = () => {
-  return <Task task={{ ...taskData }} {...actionsData} />;
+  return <Task task={object('task', { ...taskData })} {...actionsData} />;
 };
 
 export const Pinned = () => (
@@ -31,4 +35,8 @@ export const Pinned = () => (
 
 export const Archived = () => (
   <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+);
+
+export const LongTitle = () => (
+  <Task task={{ ...taskData, title: longTitleString }} {...actionsData} />
 );
